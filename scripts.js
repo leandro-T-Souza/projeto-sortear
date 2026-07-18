7e98a9afaa6efaf2273da2093947957b3d7a232a
Leandro-T-Souza <leandropop9@gmail.com>
2026-07-18 16:26:42 -0300

    Primeiro commit - projeto sortear

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function generateNumber(){
+    const min = Math.ceil(document.querySelector('.input-min').value)
+    const max = Math.floor(document.querySelector('.input-max').value)
+
+    if(min >= max ){
+        alert("O valor mino tem q ser menor que o valor máximo")
+    } else {
+          const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+     alert(result)
+    }
+}
